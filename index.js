1a87cb3fbba6807138b1974be14c43a184744d9e
Garima Khatri <84940658+GK-2211@users.noreply.github.com>
2024-01-15 06:38:11 +0530

    cors_policy

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const Seat = require('./models/seat');
 const app = express();
 const PORT = process.env.PORT || 3005;
 
+app.use("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Content-Disposition", "application/binary");
+    res.header("X-Frame-Options", "sameorigin");
+    if (req.method == "OPTIONS") {
+      res.send(200);
+    } else {
+      next();
+    }
+  });
+
 app.use(express.json());
 
 connectToDatabase(); // Connect to MongoDB
